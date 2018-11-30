f17870a43d10a411a5eddc18da2550e8899e912e
Felix Chan <felixchan29@gmail.com>
2018-11-30 15:41:05 -0500

    change ws to more secrue wss

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const API_ROOT = 'https://game-on-backend.herokuapp.com';
-export const API_WS_ROOT = 'ws://game-on-backend.herokuapp.com/cable';
+export const API_WS_ROOT = 'wss://game-on-backend.herokuapp.com/cable';
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
