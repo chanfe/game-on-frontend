971f88224868c72bcf9341b5b6c847862dae1cb7
Felix Chan <felixchan29@gmail.com>
2018-11-30 09:34:59 -0500

    change localhost to heroku and import fixed game

--- src/actions/scoreActions.js
+++ src/actions/scoreActions.js
@@ -1,5 +1,4 @@
-export const API_ROOT = 'http://localhost:3000';
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
+export const API_ROOT = 'https://game-on-backend.herokuapp.com';
 export const HEADERS = {
   'Content-Type': 'application/json',
   'Accept': 'application/json',
